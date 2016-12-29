5fe775215b9816de858fdce9ae66ef49faffe504
chenxun <chenxun1990@126.com>
2016-12-29 14:01:45 +0800

    upload latest static framework
    Signed-off-by: chenxun <chenxun1990@126.com>

---
+++ SunbeamLogService-0.1.5/ios/SunbeamLogService.framework/Versions/A/Headers/SLog.h
@@ -0,0 +1,42 @@
+//
+//  SLog.h
+//  Pods
+//
+//  Created by sunbeam on 16/7/28.
+//
+//
+
+#import <Foundation/Foundation.h>
+#import "SLogManager.h"
+
+#define SLOG_VERSION @"0.1.5"
+
+#define SLogVerbose(format, ...) SLog_Verbose(format, ##__VA_ARGS__)
+
+#define SLogDebug(format, ...) SLog_Debug(format, ##__VA_ARGS__)
+
+#define SLogInfo(format, ...) SLog_Info(format, ##__VA_ARGS__)
+
+#define SLogWarn(format, ...) SLog_Warn(format, ##__VA_ARGS__)
+
+#define SLogError(format, ...) SLog_Error(format, ##__VA_ARGS__)
+
+@interface SLog : NSObject
+
+/**
+*  初始化SLog服务
+*
+*  @param logOn DEBUG时日志是否开启
+*
+*  @return 初始化结果 0－成功；-1－失败
+*/
++ (void) initSLogService:(BOOL) logOn;
+
+/**
+ 获取log文件本地路径
+
+ @return log文件本地路径
+ */
++ (NSString *) getLogFilePath;
+
+@end
---
+++ SunbeamLogService-0.1.5/ios/SunbeamLogService.framework/Versions/A/Headers/SLogManager.h
@@ -0,0 +1,51 @@
+//
+//  SLogManager.h
+//  Pods
+//
+//  Created by sunbeam on 2016/12/28.
+//
+//
+
+#import <Foundation/Foundation.h>
+#import "SConsoleLogManager.h"
+#import "SFileLogManager.h"
+
+#define SLog_Verbose(frmt, ...) DDLogVerbose(frmt, ##__VA_ARGS__)
+
+#define SLog_Debug(frmt, ...) DDLogDebug(frmt, ##__VA_ARGS__)
+
+#define SLog_Info(frmt, ...) DDLogInfo(frmt, ##__VA_ARGS__)
+
+#define SLog_Warn(frmt, ...) DDLogWarn(frmt, ##__VA_ARGS__)
+
+#define SLog_Error(frmt, ...) DDLogError(frmt, ##__VA_ARGS__)
+
+#define LOG_LEVEL_DEF DDLogLevelVerbose
+
+#define LOG_ASYNC_ENABLED YES
+
+@interface SLogManager : NSObject
+
+/**
+ 控制台日志
+ */
+@property (nonatomic, copy, readonly) SConsoleLogManager* consoleLogManager;
+
+/**
+ 文件日志
+ */
+@property (nonatomic, copy, readonly) SFileLogManager* fileLogManager;
+
+/**
+ 单例
+
+ @return SLogManager
+ */
++ (instancetype) shareSLogManagerInstance;
+
+/**
+ 初始化SLogManager
+ */
+- (void) initSLogManager:(BOOL) logOn;
+
+@end
